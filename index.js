b2f3090116bd6855c751cb5376cc1bc9c3a82d9d
Explorer <propokerstar90@gmail.com>
2018-11-24 15:33:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function createStore() {
+    let state;
+    let listeners = [];
+    const getState =()=>state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    };
+
+    return{
+        getState,
+        subscribe
+    }
+}
