85cfb0a652e9c92699a8bfc819465297b035521d
Explorer <propokerstar90@gmail.com>
2018-11-24 18:08:36 +0200

    Extended reducer function

--- index.js
+++ index.js
@@ -33,14 +33,22 @@ let asd = {
 function todos(state = [], action) {
     if(action.type === 'ADD_TODO'){
         return state.concat([action.todo])
+    } else if(action.type === 'REMOVE_TODO'){
+        return state.filter((todo) => todo.id !== action.id)
+    } else if(action.type === 'TOGGLE_TODO'){
+        return state.map((todo) => todo.id !== action.id ? todo : {
+            name: todo.name,
+            id: todo.id,
+            complete: !todo.complete
+        })
     }
     return state
 }
 
-function createStore() {
+function createStore(reducer) {
     let state;
     let listeners = [];
-    const getState =()=>state;
+    const getState = () =>state;
     const subscribe = (listener) => {
         listeners.push(listener);
         return () => {
@@ -49,8 +57,8 @@ function createStore() {
     };
 
     const dispatch = (action) => {
-      state = todos(state, action);
-      listeners.forEach((listener) => listener())
+      state = reducer(state, action);
+      listeners.forEach((listener) => listener());
     };
 
     return{
@@ -60,7 +68,7 @@ function createStore() {
     }
 }
 
-const store = createStore();
+const store = createStore(todos);
 store.dispatch({
     type: 'ADD_TODO',
     todo: {
