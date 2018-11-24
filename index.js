ebd38ea7522f53bfcaee5a83e548bf7678a6c6ce
Explorer <propokerstar90@gmail.com>
2018-11-24 16:26:10 +0200

    Dispatch

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+let x  = {
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Keep moving forward',
+        complete: false
+    }
+};
+
+let qwe = {
+    type: 'REMOVE_TODO',
+    id: 0
+};
+
+let y = {
+    type: 'TOGGLE_TODO',
+    id: 0
+};
+
+let z = {
+    type: 'ADD_GOAL',
+    goal:{
+        id: 0,
+        name: 'Test goal'
+    }
+};
+
+let asd = {
+    type: 'REMOVE_GOAL',
+    id: 0
+};
+
+function todos(state = [], action) {
+    if(action.type === 'ADD_TODO'){
+        return state.concat([action.todo])
+    }
+    return state
+}
+
 function createStore() {
     let state;
     let listeners = [];
@@ -9,8 +48,24 @@ function createStore() {
         }
     };
 
+    const dispatch = (action) => {
+      state = todos(state, action);
+      listeners.forEach((listener) => listener())
+    };
+
     return{
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
+
+const store = createStore();
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Keep moving forward',
+        complete: false
+    }
+});
