c7e14d5a57ad0e29f1d9cc8297c1cb9f9f1bedab
Explorer <propokerstar90@gmail.com>
2018-11-25 14:03:11 +0200

    Improved reducer function

--- index.js
+++ index.js
@@ -1,65 +1,9 @@
-let x  = {
-    type: 'ADD_TODO',
-    todo: {
-        id: 0,
-        name: 'Keep moving forward',
-        complete: false
-    }
-};
-
-let qwe = {
-    type: 'REMOVE_TODO',
-    id: 0
-};
-
-let y = {
-    type: 'TOGGLE_TODO',
-    id: 0
-};
-
-let z = {
-    type: 'ADD_GOAL',
-    goal:{
-        id: 0,
-        name: 'Test goal'
-    }
-};
-
-let asd = {
-    type: 'REMOVE_GOAL',
-    id: 0
-};
-
-function todos(state = [], action) {
-    switch (action.type){
-        case 'ASS_TODO' :
-            return state.concat([action.todo]);
-        case 'REMOVE_TODO' :
-            return state.filter((todo) => todo.id !== action.id);
-        case 'TOGGLE_TODO' :
-            return state.map((todo) => todo.id !== action.id ? todo :
-                Object.assign({}, todo, {complete: !todo.complete})
-            );
-        default :
-            return state
-    }
-}
-
-function goals(state = [], action) {
-    switch (action.type){
-        case 'ADD_GOAL' :
-            return state.concat([action.goal]);
-        case 'REMOVE-GOAL' :
-            return state.filter((goal) => goal.id !== action.id);
-        default :
-            return state
-    }
-}
-
 function createStore(reducer) {
     let state;
     let listeners = [];
+
     const getState = () => state;
+
     const subscribe = (listener) => {
         listeners.push(listener);
         return () => {
@@ -79,3 +23,104 @@ function createStore(reducer) {
         dispatch
     }
 }
+
+// App
+const ADD_TODO = 'ADD_TODO';
+const REMOVE_TODO = 'REMOVE_TODO';
+const TOGGLE_TODO = 'TOGGLE_TODO';
+const ADD_GOAL = 'ADD_GOAL';
+const REMOVE_GOAL = 'REMOVE_GOAL';
+
+function addTodoAction(todo) {
+    return{
+        type: ADD_TODO,
+        todo
+    }
+}
+
+function removeTodoAction(id) {
+    return{
+        type: REMOVE_TODO,
+        id
+    }
+}
+
+function toggleTodoAction(id) {
+    return{
+        type: TOGGLE_TODO,
+        id
+    }
+}
+
+function addGoalAction(goal) {
+    return{
+        type: ADD_GOAL,
+        goal
+    }
+}
+
+function removeGoalAction(id) {
+    return{
+        type: REMOVE_GOAL,
+        id
+    }
+}
+
+
+function todos(state = [], action) {
+    switch (action.type){
+        case ADD_TODO :
+            return state.concat([action.todo]);
+        case REMOVE_TODO :
+            return state.filter((todo) => todo.id !== action.id);
+        case TOGGLE_TODO :
+            return state.map((todo) => todo.id !== action.id ? todo :
+                Object.assign({}, todo, {complete: !todo.complete})
+            );
+        default :
+            return state
+    }
+}
+
+function goals(state = [], action) {
+    switch (action.type){
+        case ADD_GOAL :
+            return state.concat([action.goal]);
+        case REMOVE_GOAL :
+            return state.filter((goal) => goal.id !== action.id);
+        default :
+            return state
+    }
+}
+
+function app(state = {}, action) {
+    return{
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
+const store = createStore(app);
+
+store.dispatch(addTodoAction({
+    id: 1,
+    name: 'Go away',
+    complete: false
+}));
+
+store.dispatch(addTodoAction({
+    id: 2,
+    name: 'Hi there',
+    complete: false
+}));
+
+store.dispatch(removeTodoAction(1));
+store.dispatch(toggleTodoAction(2));
+
+store.dispatch(addGoalAction({
+    id: 1,
+    name: 'Test',
+    complete: false
+}));
+
+store.dispatch(removeGoalAction(1));
